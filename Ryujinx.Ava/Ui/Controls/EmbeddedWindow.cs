sonnxnew0612/MeloNX-Custom-Build

commit 5bfad6865a8be9fccd54a9da080498084dc3f4fb

ava: Fix invisible vulkan window on Linux (#3901)
Co-authored-by: emmauss <emmausssss@gmail.com>

Co-authored-by: emmauss <emmausssss@gmail.com>

Ryujinx.Ava/Ui/Controls/EmbeddedWindow.cs
@@ -6,8 +6,8 @@ using SPB.Graphics;
 using SPB.Platform;
 using SPB.Platform.GLX;
 using SPB.Platform.X11;
+using SPB.Windowing;
 using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Threading.Tasks;
@@ -96,11 +96,13 @@ namespace Ryujinx.Ava.Ui.Controls
         [SupportedOSPlatform("linux")]
         IPlatformHandle CreateLinux(IPlatformHandle parent)
         {
-            X11Window = PlatformHelper.CreateOpenGLWindow(FramebufferFormat.Default, 0, 0, 100, 100) as GLXWindow;
+            X11Window = new GLXWindow(new NativeHandle(X11.DefaultDisplay), new NativeHandle(parent.Handle));
 
             WindowHandle = X11Window.WindowHandle.RawHandle;
 
             X11Display = X11Window.DisplayHandle.RawHandle;
+
+            X11Window.Hide();
 
             return new PlatformHandle(WindowHandle, "X11");
         }

Ryujinx.Ava/Ui/Controls/VulkanEmbeddedWindow.cs
@@ -20,7 +20,7 @@ namespace Ryujinx.Ava.Ui
             }
             else if (OperatingSystem.IsLinux())
             {
-                _window = X11Window;
+                _window = new SimpleX11Window(new NativeHandle(X11Display), new NativeHandle(WindowHandle));
             }
             else
             {

commit 38225870f00b7b7679d42df6a69731a09e798a2d

Avalonia: Fix OpenGL crashing on Linux (#3902)
* ava: Fix OpenGL crashing on Linux

Fixes a regression from #3901

* Fix formatting

Ryujinx.Ava/Ui/Controls/EmbeddedWindow.cs
@@ -15,12 +15,12 @@ using static Ryujinx.Ava.Ui.Controls.Win32NativeInterop;
 
 namespace Ryujinx.Ava.Ui.Controls
 {
-    public unsafe class EmbeddedWindow : NativeControlHost
+    public class EmbeddedWindow : NativeControlHost
     {
         private WindowProc _wndProcDelegate;
         private string _className;
 
-        protected GLXWindow X11Window { get; private set; }
+        protected GLXWindow X11Window { get; set; }
         protected IntPtr WindowHandle { get; set; }
         protected IntPtr X11Display { get; set; }
 
@@ -94,21 +94,17 @@ namespace Ryujinx.Ava.Ui.Controls
         }
 
         [SupportedOSPlatform("linux")]
-        IPlatformHandle CreateLinux(IPlatformHandle parent)
+        protected virtual IPlatformHandle CreateLinux(IPlatformHandle parent)
         {
-            X11Window = new GLXWindow(new NativeHandle(X11.DefaultDisplay), new NativeHandle(parent.Handle));
-
+            X11Window    = PlatformHelper.CreateOpenGLWindow(FramebufferFormat.Default, 0, 0, 100, 100) as GLXWindow;
             WindowHandle = X11Window.WindowHandle.RawHandle;
-
-            X11Display = X11Window.DisplayHandle.RawHandle;
-
-            X11Window.Hide();
+            X11Display   = X11Window.DisplayHandle.RawHandle;
 
             return new PlatformHandle(WindowHandle, "X11");
         }
 
         [SupportedOSPlatform("windows")]
-        unsafe IPlatformHandle CreateWin32(IPlatformHandle parent)
+        IPlatformHandle CreateWin32(IPlatformHandle parent)
         {
             _className = "NativeWindow-" + Guid.NewGuid();
             _wndProcDelegate = WndProc;
@@ -144,7 +140,7 @@ namespace Ryujinx.Ava.Ui.Controls
         }
 
         [SupportedOSPlatform("windows")]
-        internal IntPtr WndProc(IntPtr hWnd, WindowsMessages msg, IntPtr wParam, IntPtr lParam)
+        IntPtr WndProc(IntPtr hWnd, WindowsMessages msg, IntPtr wParam, IntPtr lParam)
         {
             var point = new Point((long)lParam & 0xFFFF, ((long)lParam >> 16) & 0xFFFF);
             var root = VisualRoot as Window;

Ryujinx.Ava/Ui/Controls/VulkanEmbeddedWindow.cs
@@ -1,16 +1,31 @@
+using Avalonia.Platform;
 using Ryujinx.Ava.Ui.Controls;
 using Silk.NET.Vulkan;
 using SPB.Graphics.Vulkan;
+using SPB.Platform.GLX;
 using SPB.Platform.Win32;
 using SPB.Platform.X11;
 using SPB.Windowing;
 using System;
+using System.Runtime.Versioning;
 
 namespace Ryujinx.Ava.Ui
 {
     public class VulkanEmbeddedWindow : EmbeddedWindow
     {
         private NativeWindowBase _window;
+
+        [SupportedOSPlatform("linux")]
+        protected override IPlatformHandle CreateLinux(IPlatformHandle parent)
+        {
+            X11Window    = new GLXWindow(new NativeHandle(X11.DefaultDisplay), new NativeHandle(parent.Handle));
+            WindowHandle = X11Window.WindowHandle.RawHandle;
+            X11Display   = X11Window.DisplayHandle.RawHandle;
+
+            X11Window.Hide();
+
+            return new PlatformHandle(WindowHandle, "X11");
+        }
 
         public SurfaceKHR CreateSurface(Instance instance)
         {