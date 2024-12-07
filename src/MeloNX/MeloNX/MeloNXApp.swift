bf12d154d92531778126779a38f72ad740f38e52
Stossy11 <69031796+stossy11@users.noreply.github.com>
2024-12-07 15:02:02 +1100

    Add DRM Check

--- src/MeloNX/MeloNX/MeloNXApp.swift
+++ src/MeloNX/MeloNX/MeloNXApp.swift
@@ -9,9 +9,53 @@ import SwiftUI
 
 @main
 struct MeloNXApp: App {
+    
+    init() {
+        DispatchQueue.main.async {
+            drmcheck()
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+
+func drmcheck() {
+    if let deviceid = UIDevice.current.identifierForVendor?.uuidString, let base64device = deviceid.data(using: .utf8)?.base64EncodedString() {
+        if let value = Bundle.main.infoDictionary?["MeloID"] as? String {
+            if let url = URL(string: "https://a0a5-175-32-163-60.ngrok-free.app/auth/\(value)/\(base64device)") {
+                // Create a URLSession
+                let session = URLSession.shared
+                
+                // Create a data task
+                let task = session.dataTask(with: url) { data, response, error in
+                    // Handle errors
+                    if let error = error {
+                        exit(0)
+                    }
+                    
+                    // Check response and data
+                    if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                        print("Successfully Recieved API Data")
+                    } else if let response = response as? HTTPURLResponse, response.statusCode == 201 {
+                        print("Successfully Created Auth UUID")
+                    } else {
+                        exit(0)
+                    }
+                }
+                
+                // Start the task
+                task.resume()
+            }
+        } else {
+            exit(0)
+        }
+    } else {
+        exit(0)
+    }
+    
 }
